41a1bbcd2b4832f7c1f12a0de72b0aff4655c7e3
cmastudios <admin@cmastudios.me>
2013-01-10 18:03:01 -0600

    Make it easier to find the group a KarmaPlayer resides in.
    Adds KarmaPlayer.getGroup() to quickly get the current KarmaGroup that a
    player is in, based off of their track and Karma point level.

--- src/main/java/com/tommytony/karma/KarmaPlayer.java
+++ src/main/java/com/tommytony/karma/KarmaPlayer.java
@@ -1,5 +1,7 @@
 package com.tommytony.karma;
 
+import org.bukkit.OfflinePlayer;
+
 public class KarmaPlayer {
 
     private final Karma karma;
@@ -88,5 +90,44 @@ public class KarmaPlayer {
      */
     public void setTrack(KarmaTrack track) {
         this.track = track;
+        if (this.getKarmaPoints() < track.getFirstGroup().getKarmaPoints()) {
+            this.addKarma(track.getFirstGroup().getKarmaPoints() - this.getKarmaPoints());
+        }
+    }
+    /**
+     * Gets the group that the player is in, based off of karma points instead
+     * of the player's permissions.
+     * @return the group
+     * @throws NullPointerException if permissions do not match karma
+     */
+    public KarmaGroup getGroup() {
+        KarmaGroup currentGroup = getTrack().getFirstGroup();
+        while (currentGroup != null) {
+            if (getKarmaPoints() >= currentGroup.getKarmaPoints()) {
+                String perm = "karma." + currentGroup.getGroupName();
+                if (getPlayer().isOnline() &&
+                        !getPlayer().getPlayer().hasPermission(perm) && currentGroup == getTrack().getFirstGroup()) {
+                    throw new NullPointerException(getName() + " does not have permissions for the start group! "
+                            + "Permissions configured incorrectly (Did you forget inheritance?).");
+                }
+                if (getPlayer().isOnline() &&
+                        !getPlayer().getPlayer().hasPermission(perm)) {
+                    // If player is online and has the karma, but lacks the permission, throw an error
+                    throw new NullPointerException(getName() + " has the karma for rank "
+                            + currentGroup.getGroupName() + " yet lacks the permission for it! "
+                            + "Permissions configured incorrectly (Did you forget inheritance?)");
+                }
+                if ((getTrack().getNextGroup(currentGroup) != null 
+                        && getKarmaPoints() < getTrack().getNextGroup(currentGroup).getKarmaPoints())
+                        || (getTrack().getNextGroup(currentGroup) == null)) {
+                    return currentGroup;
+                }
+            }
+            currentGroup = getTrack().getNextGroup(currentGroup);
+        }
+        return null;
+    }
+    public OfflinePlayer getPlayer() {
+        return karma.server.getOfflinePlayer(getName());
     }
 }
